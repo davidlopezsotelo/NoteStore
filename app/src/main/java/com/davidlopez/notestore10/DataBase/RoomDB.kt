54cb4d066288adfa097b5f75e123e3e229892457
davidlopezsotelo <davidaquarium@yahoo.es>
2023-10-31 14:52:41 +0100

    actualizacion 31/10

--- app/src/main/java/com/davidlopez/notestore10/DataBase/RoomDB.kt
+++ app/src/main/java/com/davidlopez/notestore10/DataBase/RoomDB.kt
@@ -10,7 +10,7 @@ import com.davidlopez.notestore10.DataBase.Entities.NotasEntity
 //
 
 @Database( entities = arrayOf(ContactosEntity::class,NotasEntity::class),
-                      version =1, exportSchema = false)
+                      version =1)
 
 abstract class RoomDB : RoomDatabase(){
 
